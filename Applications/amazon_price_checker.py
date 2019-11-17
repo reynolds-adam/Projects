c7e66266ee3873f897c5652467100a1c8311127a
Adam Reynolds <adamreynolds89@gmail.com>
2019-11-17 04:52:44 -0700

    Update amazon_price_checker.py

--- Applications/amazon_price_checker.py
+++ Applications/amazon_price_checker.py
@@ -1,25 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
+import smtplib
 
+# url from amazon item
 url = 'https://www.amazon.com/dp/B07G4MNFS1/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B07G4MNFS1&pd_rd_w=0tre1&pf_rd_p=45a72588-80f7-4414-9851-786f6c16d42b&pd_rd_wg=uOzSd&pf_rd_r=28TPM13C7R936AHWYJHN&pd_rd_r=6a717b03-9742-43fa-9980-b0adeee91ffc&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExSDdBQ1pOQUZMTlJTJmVuY3J5cHRlZElkPUExMDA4ODA5M1BLRlRBMk01SldINyZlbmNyeXB0ZWRBZElkPUEwNzM1MDE1MkNFWkJORFZBMUpXNCZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
 
+# User agent found by searching "my user agent on Google"
 headers = {
-    "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'}
+    "<My User Agent>'}
 
 page = requests.get(url, headers=headers)
 
-# soup = BeautifulSoup(page.content, 'html.parser')
+# function to check if the price of the item is below a desired threshold and sends an email if so
+def check_price():
+    soup1 = BeautifulSoup(page.content, "html.parser")
+    soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
 
-# title = soup.find(id="productTitle")
+    email_title = soup2.find(id= "productTitle").get_text().strip()
+    price = soup2.find(id="priceblock_ourprice").get_text().strip()
+    converted_price = float(price[1:])
 
-soup1 = BeautifulSoup(page.content, "html.parser")
+    # price check of desired price 
+    if converted_price > 340:
+        send_email(title = email_title, price = price)
 
-soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
+# function that sends the email
+def send_email(title, price):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.ehlo()
 
-# title = soup2.find(id= "productTitle").get_text()
-price = soup2.find(id="priceblock_ourprice").get_text()
-# price.stri
+    # login to email using two-step google verification app password
+    server.login('adamreynoldsdata@gmail.com', ,'<password>')
 
-print(price.strip())
+    subject = 'Price Drop to ' + str(price) +' on Sony Headphones'
+    body = 'Check the Amazon link: ' + str(url)
+    msg = f"Subject: {subject} \n\n {body}"
 
-price.strip
+    server.sendmail(
+        from_addr = 'adamreynoldsdata@gmail.com',
+        to_addrs = 'adamreynolds89@gmail.com',
+        msg = msg
+    )
+    print('Email has been sent!')
+
+
+check_price()
